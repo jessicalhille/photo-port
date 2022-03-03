f0108d3df35195981671e137c1c7b7e648fda6e0
Jessica Hille <jessicalhille@gmail.com>
2022-03-02 19:09:53 -0600

    contact rendered, test passed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from './components/Gallery'
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -16,17 +17,28 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  // set to false to prevent the contact form from showing when a user navigates to the homepage
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+          {!contactSelected ? (
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+              <ContactForm></ContactForm>
+          )}
         </div>
       </main>
     </div>
